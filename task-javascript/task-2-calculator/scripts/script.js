c2039dbe10ab2b1bbbb9aa9fac20f1bc6c2c0f47
dipenmagdani <dipenmagdani@gmail.com>
2025-01-09 17:30:02 +0530

    Completed Calculator

--- task-javascript/task-2-calculator/scripts/script.js
+++ task-javascript/task-2-calculator/scripts/script.js
@@ -1,21 +1,25 @@
 const percentage = document.getElementById("percentage");
 const userInput = document.querySelector(".userInput");
 const result = document.querySelector(".result");
+const mainData = document.querySelector(".main-data");
+let userData = "";
 
 const getValue = (value) => {
   const data = (userInput.innerHTML += value);
-  localStorage.setItem("percentage", data);
+  userData = data;
 };
-
 const calculate = () => {
-  // switch(userInput.innerHTML) {
-  //     case '%':
-
-  // }
-  if (userInput.innerHTML === "") {
+  if (userInput.innerHTML === "" || !/[+\-*/%]/.test(userData)) {
     result.innerHTML = 0;
   } else {
     result.innerHTML = eval(userInput.innerHTML);
+    result.innerHTML = result.innerHTML.substring(0, 5);
+    // result.innerHTML = c;
+    const resultData = document.createElement("span");
+    userData = userData + "=" + result.innerHTML.substring(0, 5);
+    resultData.innerHTML += userData;
+    mainData.appendChild(resultData);
+    mainData.appendChild(document.createElement("br"));
     userInput.innerHTML = "";
   }
 };
