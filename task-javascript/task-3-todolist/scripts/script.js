16227ef988c33707986d504502836c9938d15dec
dipenmagdani <dipenmagdani@gmail.com>
2025-01-10 16:59:56 +0530

    Updated the UI and added all functionalities

--- task-javascript/task-3-todolist/scripts/script.js
+++ task-javascript/task-3-todolist/scripts/script.js
@@ -1,15 +1,36 @@
 const input = document.querySelector("#taskInput");
-const mainList = document.querySelector(".list");
 const todoDiv = document.querySelector(".todo-list");
+const mainList = document.querySelector(".list");
+const emptyList = document.querySelector(".empty-list");
+
+let marked = false;
 const addTask = () => {
+  if (mainList.children[1]) {
+    emptyList.style.display = "static";
+  } else {
+    emptyList.style.display = "none";
+  }
+
+  console.log(mainList.children);
+  if (input.value === "") return alert("Input Cannot Be Empty!");
   const newLi = document.createElement("li");
   newLi.classList.add("task");
 
+  const checkIcon = document.createElement("i");
+  checkIcon.classList.add("fa-regular", "fa-circle");
+  checkIcon.style.cursor = "pointer";
+
+  const spanDiv = document.createElement("div");
+  spanDiv.classList.add("text");
   const newSpan = document.createElement("span");
   newSpan.textContent = input.value;
 
-  const checkIcon = document.createElement("i");
-  checkIcon.classList.add("fa-regular", "fa-circle");
+  spanDiv.appendChild(newSpan);
+
+  const taskInfoDiv = document.createElement("div");
+  taskInfoDiv.classList.add("task-info");
+  taskInfoDiv.appendChild(checkIcon);
+  taskInfoDiv.appendChild(spanDiv);
 
   const newDiv = document.createElement("div");
   newDiv.classList.add("manipulate");
@@ -20,8 +41,7 @@ const addTask = () => {
   const editIcon = document.createElement("i");
   editIcon.classList.add("fa-solid", "fa-pen-to-square");
 
-  newLi.appendChild(checkIcon);
-  newLi.appendChild(newSpan);
+  newLi.appendChild(taskInfoDiv);
   newDiv.appendChild(editIcon);
   newDiv.appendChild(deleteIcon);
 
@@ -30,6 +50,21 @@ const addTask = () => {
   mainList.appendChild(newLi);
 
   todoDiv.appendChild(mainList);
+
+  const todoItems = [
+    {
+      id: Date.now(),
+      taskData: input.value,
+      checkedItem: false,
+    },
+  ];
+
+  const storedInputs = JSON.parse(localStorage.getItem("todoTask")) || [];
+
+  localStorage.setItem(
+    "todoTask",
+    JSON.stringify([...storedInputs, todoItems])
+  );
   input.value = "";
 
   deleteIcon.addEventListener("click", () => {
@@ -37,12 +72,27 @@ const addTask = () => {
   });
 
   checkIcon.addEventListener("click", () => {
+    marked = true;
+    const markIcon = document.createElement("i");
+    markIcon.classList.add("fa-solid", "fa-check");
+    markIcon.style.cursor = "pointer";
+    markIcon.style.color = "rgba(255, 255, 255, 0.2)";
+
     newSpan.style.textDecoration = "line-through";
-    checkIcon.classList.remove("fa-regular", "fa-regular");
-    checkIcon.classList.add("fa-solid", "fa-check");
+    checkIcon.replaceWith(markIcon);
+
+    markIcon.addEventListener("click", () => {
+      marked = false;
+      newSpan.style.textDecoration = "none";
+      markIcon.replaceWith(checkIcon);
+    });
   });
 
   editIcon.addEventListener("click", () => {
+    if (marked) {
+      alert("Cannot Edit Checked Task!");
+      return;
+    }
     const data = prompt("Enter New Task");
     newSpan.textContent = data;
   });
