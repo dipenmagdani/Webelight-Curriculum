b19fdf19358ae371c780dddb87b824632d471c88
dipenmagdani <dipenmagdani@gmail.com>
2025-01-16 18:50:39 +0530

    third (patient) and fourth (expert care) section completed

--- bankers-heart/tailwind.config.js
+++ bankers-heart/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        sora: ["Sora", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        peach: "#FFDDB8",
+        ivory: "#FFF4E8",
+      },
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(270.06deg, #FFDDB8 0.8%, #FFF4E8 97.64%)",
+      },
+    },
   },
   plugins: [],
 };
